bf849162dc99eb4dcf0c532fafa967f6bc8dfb02
TristanSergey <tristan.sergey@gmail.com>
2024-05-16 13:02:07 +0300

    Add gitignore

--- variable.js
+++ variable.js
@@ -1,3 +1,18 @@
-﻿let name = 'serg';
-let age = 23;
-const max = Infinity;
+﻿
+
+
+let userName = 'mike'; 
+let myClass8 = 8;
+let $myVar = 'var';
+let _name =  ' Сергей '
+ 
+//не рекомендуется
+let user_name = 'serg';
+let UserName = "serg";
+
+var age = 18; //не рекомендуется.  
+
+//нельзя
+let 8myClass = 8; //начинать с цифры нельзя
+let my-Var = 'var'; //нельзя использовать дефисы, тире
+let const = '1';//зарезервированные слова нельзя
